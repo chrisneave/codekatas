a828130f0a128f447ae0f5455f3e067bd488cc3b
Chris Neave <chrispneave@gmail.com>
2013-12-12 20:54:39 +0000

    Part 3 - Making it DRY

--- ruby/kata4.rb
+++ ruby/kata4.rb
@@ -1,24 +1,19 @@
-File.open("weather.dat") do |file|
-  result = [0,100]
-  file.each do |line|
-    next if line.to_i === 0 #Skip the line if it doesn't begin with an integer
-    line_a = line.strip.split
-    temp_diff = line_a[1].to_i - line_a[2].to_i
-    next if temp_diff > result[1] #Is this the smallest level we have seen?
-    result = Array.new([line_a[0], temp_diff])
+def extract_result(filename, title_index, left_index, right_index)
+  result = [0, 100]
+  File.open(filename) do |file|
+    file.each do |line|
+      next if line.to_i === 0 #Skip the line if it doesn't begin with an integer
+      line_a = line.strip.split
+      temp_diff = line_a[left_index].to_i - line_a[right_index].to_i
+      next if temp_diff > result[1] #Is this the smallest level we have seen?
+      result = Array.new([line_a[title_index], temp_diff])
+    end
+    result
   end
-  puts "Day = #{result[0]}, Temp diff = #{result[1]}"
 end
 
-File.open("football.dat") do |file|
-  result = ["", 0]
-  file.each do |line|
-    next if line.to_i === 0 #Skip the line if it doesn't begin with an integer
-    line_a = line.strip.split
-    temp_diff = line_a[6].to_i - (line_a[8].to_i)
-    next if temp_diff < result[1] #Is this the smallest level we have seen?
-    result[0] = line_a[1]
-    result[1] = temp_diff
-  end
-  puts "Team = #{result[0]}, Goal diff = #{result[1]}"
-end
+result = extract_result("weather.dat", 0, 1, 2)
+puts "Day = #{result[0]}, Temp diff = #{result[1]}"
+
+result = extract_result("football.dat", 1, 8, 6)
+puts "Team = #{result[0]}, Goal diff = #{result[1] * -1}"
