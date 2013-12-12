62658971bcdccfa2f2078cbed34bba1b41581cd7
Chris Neave <chrispneave@gmail.com>
2013-12-12 20:35:48 +0000

    Part 2 - Football Data

--- ruby/kata4.rb
+++ ruby/kata4.rb
@@ -9,3 +9,16 @@ File.open("weather.dat") do |file|
   end
   puts "Day = #{result[0]}, Temp diff = #{result[1]}"
 end
+
+File.open("football.dat") do |file|
+  result = ["", 0]
+  file.each do |line|
+    next if line.to_i === 0 #Skip the line if it doesn't begin with an integer
+    line_a = line.strip.split
+    temp_diff = line_a[6].to_i - (line_a[8].to_i)
+    next if temp_diff < result[1] #Is this the smallest level we have seen?
+    result[0] = line_a[1]
+    result[1] = temp_diff
+  end
+  puts "Team = #{result[0]}, Goal diff = #{result[1]}"
+end
